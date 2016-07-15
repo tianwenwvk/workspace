167aa7acf30a6e0c08ffd4ef5f62c7b8d22bc3be
vk <tianwenw.vk@gmail.com>
2016-07-15 17:24:44 +0800

    Revert all "Revert "secureconnection draft""
    This reverts commit be3eda14b8f2765f4aaa64cf8af8ca9035d3fbcd.

---
+++ SecureConnectionTest/SecureConnectionTest.cpp
@@ -0,0 +1,15 @@
+//============================================================================
+// Name        : SecureConnectionTest.cpp
+// Author      : 
+// Version     :
+// Copyright   : Your copyright notice
+// Description : Hello World in C, Ansi-style
+//============================================================================
+
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(void) {
+	puts("main test");
+	return EXIT_SUCCESS;
+}
